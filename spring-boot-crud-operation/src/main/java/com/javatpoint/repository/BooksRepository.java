e217c39cce1e04512e92aa902c88c1776420bbde
vikascodepolicy <vikas.ghodke@codepolicy.com>
2021-01-31 08:42:08 -0800

    crudapplication

---
+++ spring-boot-crud-operation/src/main/java/com/javatpoint/repository/BooksRepository.java
@@ -0,0 +1,10 @@
+package com.javatpoint.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+//repository that extends CrudRepository
+import com.javatpoint.model.Books;
+@Repository
+public interface BooksRepository extends JpaRepository<Books, Integer>
+{
+}
